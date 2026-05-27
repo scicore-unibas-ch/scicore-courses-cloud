12c83460227c3651ec5ea670fa368d7119c23b7e
Pablo Escobar Lopez <pescobar001@gmail.com>
2026-05-27 18:43:37 +0200

    Fix idempotency in the tofu code for login node

--- opentofu/modules/compute_node/main.tf
+++ opentofu/modules/compute_node/main.tf
@@ -23,7 +23,7 @@ resource "openstack_compute_instance_v2" "instance" {
 
   # Security groups are attached at the port level for floating-IP nodes (see
   # openstack_networking_port_v2 above). For internal nodes they go here.
-  security_groups = var.attach_floating_ip ? [] : var.security_group_names
+  security_groups = var.attach_floating_ip ? null : var.security_group_names
 
   block_device {
     uuid                  = openstack_blockstorage_volume_v3.boot.id
